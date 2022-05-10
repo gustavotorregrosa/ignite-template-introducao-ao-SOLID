d1330e5c2e31a834e4e87fbffb306adccb2c6e56
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2022-05-10 08:57:16 -0300

    parcial

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,17 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  public id: string;
+  public admin: boolean;
+  public created_at: Date;
+  public updated_at: Date;
+
+  public constructor(public name: string, public email: string) {
+    this.id = uuidV4()
+    this.admin = false
+    this.updated_at = new Date()
+    this.created_at = new Date()
+  }
 }
 
 export { User };
